658773b48f3130af20c48dc3b0acae882232f1cb
Samima Akter Tithy <95161362+samima-789@users.noreply.github.com>
2023-11-22 01:22:03 +0600

    Add contact page and updated title part.

--- js/script.js
+++ js/script.js
@@ -19,3 +19,13 @@ window.onscroll = () =>{
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
 }*/
+            const header = document.querySelector("header");
+            window.addEventListener ("scroll", function(){
+            header.classList.toggle ("sticky", window.scrollY > 0)
+            });
+
+            $(document).ready(function(){
+                $(".navigation .navbar .logo-toggle-container .toggle-box").click(function(){
+                    $(".navigation .menu").slideToggle();
+                });
+            });
