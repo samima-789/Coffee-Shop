e7c37c6bd3993978749f4d1949e5afa0fd34be78
Samima Akter Tithy <95161362+samima-789@users.noreply.github.com>
2024-01-14 01:13:01 +0600

    Add review section.

--- js/script.js
+++ js/script.js
@@ -1,31 +1,11 @@
-/*let navbar = document.querySelector('.navbar');
-
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
-}
-
-let searchForm = document.querySelector('.search');
-
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-    navbar.classList.remove('active');
-    cartItem.classList.remove('active');
-}
-
-window.onscroll = () =>{
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
-}*/
-            const header = document.querySelector("header");
-            window.addEventListener ("scroll", function(){
-            header.classList.toggle ("sticky", window.scrollY > 0)
+const header = document.querySelector("header");
+    window.addEventListener ("scroll", function(){
+        header.classList.toggle ("sticky", window.scrollY > 0)
+    });
+        $(document).ready(function(){
+            $(".navigation .navbar .logo-toggle-container .toggle-box").click(function(){
+                $(".navigation .menu").slideToggle();
             });
+});
 
-            $(document).ready(function(){
-                $(".navigation .navbar .logo-toggle-container .toggle-box").click(function(){
-                    $(".navigation .menu").slideToggle();
-                });
-            });
+
