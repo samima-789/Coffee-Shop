bd89bf4b7892cbaf9dc22f8ea34c109bc16d1a27
Samima Akter Tithy <95161362+samima-789@users.noreply.github.com>
2024-01-15 01:28:50 +0600

    Add Team Member section, Review Section and Others section and add Tailwind CSS.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-const header = document.querySelector("header");
-    window.addEventListener ("scroll", function(){
-        header.classList.toggle ("sticky", window.scrollY > 0)
-    });
-        $(document).ready(function(){
-            $(".navigation .navbar .logo-toggle-container .toggle-box").click(function(){
-                $(".navigation .menu").slideToggle();
-            });
-});
+
 
 
