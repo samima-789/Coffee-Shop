578a4812733e945c52a688e18e35acf41c9671ca
Samima Akter Tithy <tithysamima789@gmail.com>
2024-06-05 01:29:57 +0600

    Added new section.

--- assets/js/script.js
+++ assets/js/script.js
@@ -163,3 +163,48 @@ $(document).ready(function(){
      });
    }
  });
+
+ /*-- Isotop --*/
+var $grid = $('.item-wrap-details').isotope({
+});
+$('.button-group-wrap').on('click', 'li', function(){
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+
+/*-- Slick Slider */
+$('.slider').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
